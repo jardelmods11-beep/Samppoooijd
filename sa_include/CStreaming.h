82003393b5e66393e44f92cf50419f7fdf9f00da
jardelmods11-beep <jardelmods11@gmail.com>
2026-05-17 10:15:12 -0300

    Modified by www.SourceFiles.app

---
+++ sa_include/CStreaming.h
@@ -0,0 +1,11 @@
+#ifndef __CSTREAMING_H__
+#define __CSTREAMING_H__
+
+class CStreaming
+{
+public:
+	static int RequestModel(int, int);
+	static int RequestSpecialModel(int, const char*, int);
+	static int LoadAllRequestedModels(bool);};
+
+#endif
