7df91e923d907c39f503741df8f568260591688b
jardelmods11-beep <jardelmods11@gmail.com>
2026-05-17 10:15:15 -0300

    Modified by www.SourceFiles.app

---
+++ sa_include/CLoadingScreen.h
@@ -0,0 +1,12 @@
+#ifndef __CLOADINGSCREEN_H__
+#define __CLOADINGSCREEN_H__
+
+class CLoadingScreen
+{
+public:
+	static void NewChunkLoaded();
+};
+
+void LoadingScreen(char const*, char const*, char const*);
+
+#endif
