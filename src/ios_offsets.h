4716dfdaff22c88980d61cd0e0d1e13771629a86
jardelmods11-beep <jardelmods11@gmail.com>
2026-05-17 15:48:27 -0300

    Modified by www.SourceFiles.app

---
+++ src/ios_offsets.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "ios_base.h"
+
+// Offsets reais do gta3sa ARM64
+#define OFFSET_FINDPLAYERPED                     0x004A5DE0UL
+#define OFFSET_CFONT_INITIALISE                  0x0037C114UL
+#define OFFSET_CFONT_PRINTSTRING                 0x0037EBC4UL
+#define OFFSET_CHUD_DRAWAFTERFADE                0x002A67D4UL
+#define OFFSET_RENDER2DSTUFFAFTERFADE            0x002A9294UL
+#define OFFSET_CCREDITS_RENDER                   0x002A92A4UL
+#define OFFSET_CMESSAGES_DISPLAY                 0x002428E4UL
+#define OFFSET_LOADINGSCREEN_FUNC                0x0024017CUL
+#define OFFSET_CSTREAMING_INIT                   0x002EB31CUL
+#define OFFSET_CSTREAMING_LOADALLREQUESTEDMODELS 0x002EB31CUL
+#define OFFSET_CSTREAMING_SETUP                  0x002F9880UL
+#define OFFSET_CDSTREAM_INIT                     0x00160798UL
+#define OFFSET_CRUNNINGSCRIPT_PROCESS            0x001CFF1CUL
+#define OFFSET_CPOPCYCLE_UPDATE                  0x002F7894UL
+#define OFFSET_PEDEVENT_INIT                     0x0035BBE0UL
+#define OFFSET_CCLOTHES_REBUILDPLAYER            0x002DED20UL
+#define OFFSET_SAVELOAD                          0x002D4B84UL
+#define OFFSET_TOUCHINPUT                        0x0014505CUL
+#define OFFSET_GGAMESTATE_FUNC                   0x004B7E70UL
